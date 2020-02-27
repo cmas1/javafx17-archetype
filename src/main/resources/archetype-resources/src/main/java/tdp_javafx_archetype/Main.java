471ca6daa20b1f9c403c216f6eb02c985b47b2fe
Alberto Monge <alberto.monge@polito.it>
2020-02-27 10:46:42 +0100

    first commit

---
+++ src/main/resources/archetype-resources/src/main/java/tdp_javafx_archetype/Main.java
@@ -0,0 +1,10 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.tdp_javafx_archetype;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
